0a113b13732f866134f40476f05658f2c9a2941f
kobi32768 <kobi32768@gmail.com>
2024-01-09 15:46:08 +0900

    chore: update message

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4413: Hello Argo CD")
+        return c.String(http.StatusOK, "4413: Hello automatic Argo CD")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4413", "message": "Hello Argo CD"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4413", "message": "Hello automatic Argo CD"})
     }
 }
